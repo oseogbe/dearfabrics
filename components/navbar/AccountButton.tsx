50d8bdb940a65308a5b9354845e59c8b0608f787
Ose Ogbe <therealnylat@gmail.com>
2024-03-24 07:05:15 +0100

    site header

---
+++ components/navbar/AccountButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { FiUser } from "react-icons/fi";
+
+const AccountButton = () => {
+    return (
+        <div
+            className="p-3 md:p-4 bg-[#F6F6F6] rounded-lg cursor-pointer"
+            onClick={() => { }}
+        >
+            <FiUser className="w-4 h-4 md:w-5 md:h-5 text-[#807D7E]" />
+        </div>
+    )
+}
+
+export default AccountButton
---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+    const router = useRouter();
+
+    return (
+        <Image
+            src="/img/dfng-logo.png"
+            alt="dear fabrics logo"
+            height={100}
+            width={100}
+            className="cursor-pointer h-9 md:h-12 w-auto"
+            onClick={() => router.push('/')}
+        />
+    )
+}
+
+export default Logo
