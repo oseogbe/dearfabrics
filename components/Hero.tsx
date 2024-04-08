8b7a49e3f0489cea6ed9fc625f47cba2d172b2dc
Ose Ogbe <therealnylat@gmail.com>
2024-04-08 13:06:36 +0100

    ankara card and categories sections

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -54,8 +54,8 @@ const Hero = () => {
                             <div className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 xl:grid-cols-2">
                                 <div className="grid grid-cols-5 md:grid-cols-6 xl:grid-cols-4">
                                     <div className="col-start-2 col-span-3 md:col-start-2 md:col-span-4 xl:col-start-2 xl:col-span-2 flex flex-col gap-6 text-center text-white mt-12 mb-8">
-                                        <div className="text-sm md:text-base xl:text-left xl:text-lg font-semibold">{slide.category}</div>
-                                        <div className="text-[26px] leading-8 xl:text-left xl:text-4xl xl:leading-10 font-black">{slide.text}</div>
+                                        <h5 className="text-sm md:text-base xl:text-left xl:text-lg font-semibold">{slide.category}</h5>
+                                        <h3 className="text-[26px] leading-8 xl:text-left xl:text-4xl xl:leading-10 font-black">{slide.text}</h3>
                                         <div className="text-base xl:text-left xl:text-xl font-semibold">{slide.subtext}</div>
                                         <button
                                             className="bg-black w-[134px] xl:w-[200px] mx-auto xl:mx-0 text-sm xl:text-lg font-extrabold p-3 xl:p-4 rounded-md xl:rounded-lg"
