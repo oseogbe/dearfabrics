2015ad107675a37d29d0e50f6912fcf4e074c9dc
Ose Ogbe <therealnylat@gmail.com>
2024-04-29 18:33:31 +0100

    search update

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -1,10 +1,25 @@
 "use client"
 
-import { useSearchParams } from "next/navigation"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+
+import { useDebouncedCallback } from 'use-debounce'
+
 import { IoSearch } from "react-icons/io5"
 
 const Search = () => {
-    const params = useSearchParams()
+    const searchParams = useSearchParams()
+    const pathname = usePathname()
+    const { replace } = useRouter()
+
+    const handleSearch = useDebouncedCallback((term) => {
+        const params = new URLSearchParams(searchParams)
+        if (term) {
+            params.set('query', term)
+        } else {
+            params.delete('query')
+        }
+        replace(`${pathname}?${params.toString()}`)
+    }, 300)
 
     return (
         <div>
@@ -13,6 +28,10 @@ const Search = () => {
                     type="search"
                     className="w-80 ps-14 pe-5 py-3 bg-[#F6F6F6] text-[#807D7E] rounded-lg focus-visible:outline-none"
                     placeholder="Search"
+                    onChange={(e) => {
+                        handleSearch(e.target.value)
+                    }}
+                    defaultValue={searchParams.get('query')?.toString()}
                 />
 
                 <IoSearch className="absolute top-3.5 left-5 w-5 h-5 text-[#807D7E]" />
