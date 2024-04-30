f6f071269e24644ba27e5bc411cce3ccd88440eb
Ose Ogbe <osememen.ogbe@gmail.com>
2024-04-30 16:47:12 +0100

    collections page update

---
+++ components/Pagination.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+interface PaginationProps {
+
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+
+}) => {
+    return (
+        <nav className="flex items-center gap-x-1">
+            <button type="button" className="min-h-[54px] min-w-[54px] py-2 px-2.5 inline-flex jusify-center items-center gap-x-2 text-xl rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+                <svg className="flex-shrink-0 size-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m15 18-6-6 6-6"></path>
+                </svg>
+                <span aria-hidden="true" className="sr-only">Previous</span>
+            </button>
+            <div className="flex items-center gap-x-1">
+                <button type="button" className="min-h-[54px] min-w-[54px] flex justify-center items-center bg-gray-200 text-gray-800 py-2 px-3 text-xl rounded-lg focus:outline-none focus:bg-gray-300 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-600 dark:text-white dark:focus:bg-neutral-500" aria-current="page">1</button>
+                <button type="button" className="min-h-[54px] min-w-[54px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-xl rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">2</button>
+                <button type="button" className="min-h-[54px] min-w-[54px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-xl rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">3</button>
+                <div className="hs-tooltip inline-block">
+                    <button type="button" className="hs-tooltip-toggle group min-h-[54px] min-w-[54px] flex justify-center items-center text-gray-400 hover:text-gray-600 p-2 text-xl rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-500 dark:hover:text-gray-500 dark:focus:bg-white/10">
+                        <span className="group-hover:hidden text-xs">•••</span>
+                        <svg className="group-hover:block hidden flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="m6 17 5-5-5-5"></path>
+                            <path d="m13 17 5-5-5-5"></path>
+                        </svg>
+                        <span className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded shadow-sm dark:bg-neutral-700" role="tooltip">
+                            Next 4 pages
+                        </span>
+                    </button>
+                </div>
+                <button type="button" className="min-h-[54px] min-w-[54px] flex justify-center items-center text-gray-800 hover:bg-gray-100 py-2 px-3 text-xl rounded-lg focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">8</button>
+            </div>
+            <button type="button" className="min-h-[54px] min-w-[54px] py-2 px-2.5 inline-flex jusify-center items-center gap-x-2 text-xl rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
+                <span aria-hidden="true" className="sr-only">Next</span>
+                <svg className="flex-shrink-0 size-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m9 18 6-6-6-6"></path>
+                </svg>
+            </button>
+        </nav>
+    )
+}
+
+export default Pagination
--- components/product/ProductFilter.tsx
+++ components/product/ProductFilter.tsx
@@ -1,18 +1,108 @@
 "use client"
 
 import { useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 import RangeSlider from 'react-range-slider-input'
 import 'react-range-slider-input/dist/style.css'
 
+import { cn } from "@/lib/utils"
+
 const ProductFilter = () => {
-    const [value, setValue] = useState([100000, 150000])
+    const [value, setValue] = useState([50000, 200000])
+
+    const searchParams = useSearchParams()
+
+    const subCategories = [
+        "Lace",
+        "Aso-oke",
+        "George",
+        "Flower",
+        "Plain",
+        "Ankara"
+    ]
+
+    const colors = [
+        {
+            name: "Purple",
+            class: "bg-purple-700"
+        },
+        {
+            name: "Black",
+            class: "bg-black"
+        },
+        {
+            name: "Red",
+            class: "bg-red-600"
+        },
+        {
+            name: "Orange",
+            class: "bg-orange-600"
+        },
+        {
+            name: "Navy",
+            class: "bg-[#345eff]"
+        },
+        {
+            name: "White",
+            class: "bg-white"
+        },
+        {
+            name: "Brown",
+            class: "bg-[#d67e3b]"
+        },
+        {
+            name: "Green",
+            class: "bg-green-500"
+        },
+        {
+            name: "Yellow",
+            class: "bg-yellow-500"
+        },
+        {
+            name: "Grey",
+            class: "bg-gray-500"
+        },
+        {
+            name: "Pink",
+            class: "bg-pink-500"
+        },
+        {
+            name: "Blue",
+            class: "bg-blue-400"
+        },
+    ]
+
+    const sizes = [
+        "XXS",
+        "XL",
+        "XS",
+        "S",
+        "M",
+        "L",
+        "XXL",
+        "3XL",
+        "4XL"
+    ]
 
     return (
         <div className="p-8 border shadow-md rounded-lg">
             <h3 className='text-[#807D7E] text-2xl font-bold'>Filter Options</h3>
             <hr className='my-8' />
-            <div>
+            <div className='space-y-10'>
+                <div className='flex flex-col gap-4'>
+                    <h4 className='text-[#807D7E] text-xl font-semibold'>Sub-categories</h4>
+                    <div className='space-y-4'>
+                        {
+                            subCategories.map(category => (
+                                <div key={category} className='flex justify-between'>
+                                    <h6 className='text-[#3C4242] font-semibold'>{category}</h6>
+                                    <input type="checkbox" className="shrink-0 border-gray-200 rounded !text-df-yellow focus:ring-df-yellow/80" />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
                 <div className='flex flex-col gap-4'>
                     <h4 className='text-[#807D7E] text-xl font-semibold'>Price</h4>
                     <div>₦{value[0]} - ₦{value[1]}</div>
@@ -21,12 +111,36 @@ const ProductFilter = () => {
                         min={50000}
                         max={200000}
                         step={10000}
-                        defaultValue={[100000, 150000]}
+                        defaultValue={[value[0], value[1]]}
                         onInput={setValue}
                     />
                 </div>
+                <div className='flex flex-col gap-4'>
+                    <h4 className='text-[#807D7E] text-xl font-semibold'>Colors</h4>
+                    <div className='grid grid-cols-4 gap-6'>
+                        {
+                            colors.map(color => (
+                                <div key={color.name}>
+                                    <div className={cn('w-full aspect-square border rounded-lg cursor-pointer', color.class)}></div>
+                                    <div className='mt-2 text-[#8A8989] text-[10px] text-center font-semibold'>{color.name}</div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    <h4 className='text-[#807D7E] text-xl font-semibold'>Sizes</h4>
+                    <div className='grid grid-cols-3 gap-5'>
+                        {
+                            sizes.map(size => (
+                                <div key={size}>
+                                    <div className='h-8 flex items-center justify-center text-sm text-[#3C4242] font-semibold border rounded-md cursor-pointer'>{size}</div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
