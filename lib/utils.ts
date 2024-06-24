32d5223cb9d8535ef4334e81e331d05d4d2a7447
Ose Ogbe <osememen.ogbe@gmail.com>
2024-06-24 01:48:23 +0100

    format currency

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,18 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatAmount(value?: number) {
-  if (!value) return ''
-  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+export function formatCurrency(amount: number) {
+  // if (!value) return ''
+  // return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  return new Intl.NumberFormat('en-NG', {
+    style: 'currency',
+    currency: 'NGN'
+  }).format(amount)
+}
+
+export function percentageDiscount(price: number, discountPrice: number) {
+  if (discountPrice) {
+    return Math.round((price - discountPrice) / price * 100)
+  }
+  return 0
 }
