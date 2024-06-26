7a097970b04b24eb55c07ed4f79a8e1cf6c884dc
Ose Ogbe <therealnylat@gmail.com>
2024-06-26 08:34:14 +0100

    cart updates

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { notFound } from 'next/navigation'
 
 import { useShoppingCart } from 'use-shopping-cart'
+import { motion } from "framer-motion"
 
 import Container from '@/components/Container'
 import OrderSummary from '@/components/cart/OrderSummary'
@@ -11,14 +12,20 @@ import OrderSummary from '@/components/cart/OrderSummary'
 import { products } from '@/lib/products'
 
 import { CheckCircleIcon, Clock3Icon } from 'lucide-react'
+import { FaRegSadTear } from 'react-icons/fa'
 import { formatCurrency } from '@/lib/utils'
+import { ProductType } from '@/typings'
 
 const CartPage = () => {
-    const { cartDetails, cartCount, incrementItem, decrementItem } = useShoppingCart()
+    const { cartDetails, cartCount, incrementItem, decrementItem, removeItem } = useShoppingCart()
+    const [cartProducts, setCartProducts] = useState<ProductType[]>([])
 
-    if (!cartDetails || !cartCount) notFound()
-
-    const cartProducts = Object.keys(cartDetails).map(key => products.find(product => product.sku === cartDetails[key].id))
+    useEffect(() => {
+        if (!cartDetails) return
+        const productIds = Object.keys(cartDetails).map(key => cartDetails[key].id)
+        const filteredProducts = products.filter(product => productIds.includes(product.sku))
+        setCartProducts(filteredProducts)
+    }, [cartDetails, cartCount])
 
     return (
         <Container>
@@ -43,10 +50,24 @@ const CartPage = () => {
                                 </div>
                             </div>
                         </div> */}
+                        {!cartDetails || !cartCount && (
+                            <div className='grid place-items-center h-[50vh]'>
+                                <div className='flex flex-col items-center'>
+                                    <div className='flex'>
+                                        <motion.div
+                                            animate={{ rotate: [-10, 10, -10] }}
+                                            transition={{ duration: 0.5, repeat: Infinity }}
+                                        >
+                                            <FaRegSadTear size={36} />
+                                        </motion.div>
+                                    </div>
+                                    <p className="text-xl md:text-2xl mt-4">Your cart is empty</p>
+                                </div>
+
+                            </div>
+                        )}
                         {
                             cartProducts.map(product => {
-                                if (!product) return
-
                                 return (
                                     <div
                                         key={product.sku}
@@ -66,7 +87,7 @@ const CartPage = () => {
                                                 <div className="flex flex-col max-[500px]:items-center gap-3">
                                                     <h6 className="font-semibold text-base leading-7 text-black">{product.name}</h6>
                                                     <h6 className="font-normal text-base leading-7 text-gray-500 capitalize">{product.category}</h6>
-                                                    <h6 className="font-medium text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-df-yellow">{formatCurrency(cartDetails[product.sku].price)}</h6>
+                                                    <h6 className="font-medium text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-df-yellow">{formatCurrency(product.price)}</h6>
                                                     <div className='flex items-center gap-x-2 text-sm text-gray-500 mt-4 xl:mt-12'>
                                                         {product?.inStock && (
                                                             <><CheckCircleIcon size={18} className='text-green-500' /> In stock</>
@@ -77,45 +98,52 @@ const CartPage = () => {
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="flex items-center py-4 max-[500px]:justify-center h-full max-md:mt-3">
-                                                <div className="flex items-start h-full">
-                                                    <button
-                                                        className="group rounded-l-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300"
-                                                        onClick={() => decrementItem(product.sku)}
-                                                    >
-                                                        <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
-                                                            xmlns="http://www.w3.org/2000/svg" width="22" height="22"
-                                                            viewBox="0 0 22 22" fill="none">
-                                                            <path d="M16.5 11H5.5" stroke="" strokeWidth="1.6"
-                                                                strokeLinecap="round" />
-                                                            <path d="M16.5 11H5.5" stroke="" strokeOpacity="0.2" strokeWidth="1.6"
-                                                                strokeLinecap="round" />
-                                                            <path d="M16.5 11H5.5" stroke="" strokeOpacity="0.2" strokeWidth="1.6"
-                                                                strokeLinecap="round" />
-                                                        </svg>
-                                                    </button>
-                                                    <input type="text"
-                                                        className="border-y border-gray-200 outline-none text-gray-900 font-semibold text-lg w-full max-w-[73px] min-w-[60px] placeholder:text-gray-900 py-[15px] text-center bg-transparent focus:border-gray-200 focus:outline-none focus:ring-0"
-                                                        value={cartDetails[product.sku].quantity} disabled />
-                                                    <button
-                                                        className="group rounded-r-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300"
-                                                        onClick={() => incrementItem(product.sku)}
-                                                    >
-                                                        <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
-                                                            xmlns="http://www.w3.org/2000/svg" width="22" height="22"
-                                                            viewBox="0 0 22 22" fill="none">
-                                                            <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" strokeWidth="1.6"
-                                                                strokeLinecap="round" />
-                                                            <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" strokeOpacity="0.2"
-                                                                strokeWidth="1.6" strokeLinecap="round" />
-                                                            <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" strokeOpacity="0.2"
-                                                                strokeWidth="1.6" strokeLinecap="round" />
-                                                        </svg>
-                                                    </button>
+                                            <div className="flex items-start py-4 max-[500px]:justify-center h-full max-md:mt-3">
+                                                <div className='grid place-items-center gap-y-4'>
+                                                    <div className="flex items-start h-full">
+                                                        <button
+                                                            className="group rounded-l-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300"
+                                                            onClick={() => decrementItem(product.sku)}
+                                                        >
+                                                            <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+                                                                xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                                                                viewBox="0 0 22 22" fill="none">
+                                                                <path d="M16.5 11H5.5" stroke="" strokeWidth="1.6"
+                                                                    strokeLinecap="round" />
+                                                                <path d="M16.5 11H5.5" stroke="" strokeOpacity="0.2" strokeWidth="1.6"
+                                                                    strokeLinecap="round" />
+                                                                <path d="M16.5 11H5.5" stroke="" strokeOpacity="0.2" strokeWidth="1.6"
+                                                                    strokeLinecap="round" />
+                                                            </svg>
+                                                        </button>
+                                                        <input type="text"
+                                                            className="border-y border-gray-200 outline-none text-gray-900 font-semibold text-lg w-full max-w-[73px] min-w-[60px] placeholder:text-gray-900 py-[15px] text-center bg-transparent focus:border-gray-200 focus:outline-none focus:ring-0"
+                                                            value={cartDetails && cartDetails[product.sku] ? cartDetails[product.sku].quantity : ''} disabled />
+                                                        <button
+                                                            className="group rounded-r-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300"
+                                                            onClick={() => incrementItem(product.sku)}
+                                                        >
+                                                            <svg className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+                                                                xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                                                                viewBox="0 0 22 22" fill="none">
+                                                                <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" strokeWidth="1.6"
+                                                                    strokeLinecap="round" />
+                                                                <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" strokeOpacity="0.2"
+                                                                    strokeWidth="1.6" strokeLinecap="round" />
+                                                                <path d="M11 5.5V16.5M16.5 11H5.5" stroke="" strokeOpacity="0.2"
+                                                                    strokeWidth="1.6" strokeLinecap="round" />
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                    {/* remove cart item */}
+                                                    <div className='text-sm underline cursor-pointer' onClick={() => removeItem(product.sku)}>remove</div>
                                                 </div>
+
                                             </div>
-                                            <div className="flex items-start md:py-8 max-[500px]:justify-center md:justify-end max-md:mt-3 h-full">
-                                                <p className="font-bold text-lg leading-8 text-gray-600 text-center transition-all duration-300 group-hover:text-df-yellow">{formatCurrency(cartDetails[product.sku].value)}</p>
+                                            <div className="flex items-start md:py-8 max-[500px]:justify-center md:justify-end h-full max-md:mt-3">
+                                                <p className="font-bold text-lg leading-8 text-gray-600 text-center transition-all duration-300 group-hover:text-df-yellow">
+                                                    {formatCurrency(product.price)}
+                                                </p>
                                             </div>
                                         </div>
                                     </div>
