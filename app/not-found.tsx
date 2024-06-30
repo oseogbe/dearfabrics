93724926e83b3ca87ea9739db4255430965315ed
Ose Ogbe <osememen.ogbe@gmail.com>
2024-06-30 22:12:46 +0100

    minor change

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,29 +2,27 @@
 
 import Link from 'next/link'
 
-import { motion, AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 
 import { FrownIcon } from 'lucide-react'
 
 export default function NotFound() {
     return (
-        <AnimatePresence>
-            <main className="flex h-full min-h-[50vh] flex-col items-center justify-center gap-2">
-                <motion.div
-                    animate={{ rotate: [-10, 10, -10] }}
-                    transition={{ duration: 0.5, repeat: Infinity }}
-                >
-                    <FrownIcon size={28} className="text-gray-400" />
-                </motion.div>
-                <h2 className="text-xl font-semibold">404 Not Found</h2>
-                <p>Could not find the requested page.</p>
-                <Link
-                    href="/"
-                    className="mt-4 rounded-md bg-df-yellow px-4 py-2 text-sm text-white transition-colors hover:bg-df-yellow/80"
-                >
-                    Go to Homepage
-                </Link>
-            </main>
-        </AnimatePresence>
+        <main className="flex h-full min-h-[50vh] flex-col items-center justify-center gap-2">
+            <motion.div
+                animate={{ rotate: [-10, 10, -10] }}
+                transition={{ duration: 0.5, repeat: Infinity }}
+            >
+                <FrownIcon size={28} className="text-gray-400" />
+            </motion.div>
+            <h2 className="text-xl font-semibold">404 Not Found</h2>
+            <p>Could not find the requested page.</p>
+            <Link
+                href="/"
+                className="mt-4 rounded-md bg-df-yellow px-4 py-2 text-sm text-white transition-colors hover:bg-df-yellow/80"
+            >
+                Go to Homepage
+            </Link>
+        </main>
     );
 }
