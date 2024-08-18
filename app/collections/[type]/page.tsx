5d6e7483a88e56b00113a02391abbce5ccb30b71
Ose Ogbe <therealnylat@gmail.com>
2024-08-18 12:34:10 +0100

    products filter update

--- app/collections/[type]/page.tsx
+++ app/collections/[type]/page.tsx
@@ -8,6 +8,7 @@ import ProductsClient from "./ProductsClient"
 
 import { fetchProductsByCategory } from "@/lib/sanity"
 import Pagination from "@/components/Pagination"
+import { ProductType } from "@/typings"
 
 // interface CollectionsProps {
 //     params: {
@@ -21,11 +22,25 @@ import Pagination from "@/components/Pagination"
 //     }
 // }
 
+const formatPrice = (num: number, upOrDown: 'up' | 'down') => {
+    const magnitude = Math.floor(Math.log10(num))
+    const base = Math.pow(10, magnitude)
+    let roundedNum
+    if (upOrDown == 'up') {
+        roundedNum = Math.ceil(num / base) * base
+    } else {
+        roundedNum = Math.floor(num / base) * base
+    }
+    return roundedNum
+}
+
 const CollectionPage = () => {
     const { type } = useParams()
     const [currentPage, setCurrentPage] = useState(1)
     const [pageSize, setPageSize] = useState(20)
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState<ProductType[]>([])
+    const [minPrice, setMinPrice] = useState(0)
+    const [maxPrice, setMaxPrice] = useState(0)
     const [total, setTotal] = useState(0)
 
     useEffect(() => {
@@ -37,6 +52,12 @@ const CollectionPage = () => {
         fetchData()
     }, [currentPage, type, pageSize])
 
+    useEffect(() => {
+        const productPrices = products.flatMap(product => product.price)
+        setMinPrice(formatPrice(Math.min(...productPrices), "down"))
+        setMaxPrice(formatPrice(Math.max(...productPrices), "up"))
+    }, [maxPrice, minPrice, products])
+
     const handlePageChange = (page: number) => {
         setCurrentPage(page)
     }
@@ -46,9 +67,10 @@ const CollectionPage = () => {
             <div className="pt-5 flex xl:gap-8 xl:pt-10">
                 <ProductsClient
                     products={products}
+                    minPrice={minPrice}
+                    maxPrice={maxPrice}
                     total={total}
                 />
-
             </div>
             <div className="mt-16 flex justify-center">
                 <Pagination
