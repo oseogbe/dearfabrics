755d92c10aef1053c7efc0c9176cbea1e8ce83a1
Osememen Ogbe <osememen.ogbe@gmail.com>
2024-12-14 08:01:47 +0100

    product quick view ui updates

--- components/product/QuickView.tsx
+++ components/product/QuickView.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -8,9 +8,9 @@ import { useShoppingCart } from "use-shopping-cart"
 import { toast } from "sonner"
 
 import { ProductType } from "@/typings"
-import { cn, formatCurrency } from "@/lib/utils"
+import { cn, formatCurrency, setCartItem } from "@/lib/utils"
 
-import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
 import ProductStars from "./ProductStars"
 
 import { ExpandIcon } from "lucide-react"
@@ -23,19 +23,15 @@ const QuickView = ({
     product: ProductType
 }) => {
     const [selectedImage, setSelectedImage] = useState(product.images[0])
-    const [selectedColor, setSelectedColor] = useState(product.colors[0])
-    const [selectedSize, setSelectedSize] = useState(product.sizes ? product.sizes[0] : "")
+    const [selectedColor, setSelectedColor] = useState(product.colors[0][0])
+    const [selectedSize, setSelectedSize] = useState(product.sizes?.[0][0])
     const [quantity, setQuantity] = useState(1)
 
     const shoppingCart = useShoppingCart()
 
     const handleAddItem = () => {
-        shoppingCart.addItem(
-            product,
-            {
-                count: quantity,
-                product_metadata: product
-            })
+        const { item, count } = setCartItem(product, selectedColor, selectedSize, quantity)
+        shoppingCart.addItem(item, { count })
         setQuantity(1)
         toast("Added to cart", { duration: 1500 })
     }
@@ -50,117 +46,106 @@ const QuickView = ({
                     <ExpandIcon size={24} className="text-black" />
                 </button>
             </DialogTrigger>
-            <DialogContent className="max-w-[95%] px-3 md:max-w-[560px] lg:max-w-[992px] max-h-[90%] overflow-y-scroll scrollbar-hide rounded-md">
-                <div className="flex flex-col lg:flex-row">
-                    <div className="flex-1">
-                        <div className="flex flex-col-reverse md:flex-row h-auto md:h-[650px] xl:h-[700px] overflow-y-hidden">
-                            <div className="flex-1 lg:ml-4 xl:mx-8 border-2 border-df-gray cursor-pointer">
-                                <Image
-                                    src={selectedImage}
-                                    alt={product.name}
-                                    width={1000}
-                                    height={1000}
-                                    className="h-full object-cover"
-                                />
+            <DialogContent className="max-w-[95%] md:max-w-[560px] lg:max-w-[992px] min-h-[calc(100dvh-100px)] lg:min-h-fit px-3 lg:px-0 overflow-y-scroll scrollbar-hide rounded-md">
+                <DialogHeader className="hidden">
+                    <DialogTitle>
+                        {product.name}
+                    </DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-col lg:flex-row lg:gap-x-8">
+                    <div className="flex-1 w-full relative -my-6">
+                        <Image
+                            src={selectedImage}
+                            alt={product.name}
+                            fill
+                            className="object-cover"
+                        />
+                    </div>
+                    <div className="flex-1 lg:pb-12">
+                        <div className="flex flex-col gap-y-6 lg:gap-y-12">
+                            <Link href={`/collections/${product.categories[0]}`} className="text-xs lg:text-sm text-df-yellow capitalize">{product.categories[0]}</Link>
+                            <Link href={`/${product.categories[0]}/${product.slug}`} className="text-lg lg:text-xl text-[#3C4242] font-bold pr-3">{product.name}</Link>
+                        </div>
+                        <div className="mt-3 lg:mt-4 text-[18px] md:text-xl text-[#3C4242] font-medium">
+                            {product.oldPrice ? (
+                                <div>
+                                    {`${formatCurrency(product.price)}`}
+                                    <span className="ml-3 text-[#848485] line-through" dangerouslySetInnerHTML={{ __html: `${formatCurrency(product.oldPrice)}` }}></span>
+                                </div>
+                            ) : (
+                                `${formatCurrency(product.price)}`
+                            )}
+                        </div>
+                        <div className="hidden lg:block mt-6 text-sm xl:text-base text-[#3C4242] pr-3">{product.description}</div>
+                        <div className="mt-6">
+                            <div className="hidden lg:block">
+                                <span className="font-bold text-[#3C4242] text-sm md:text-base xl:text-lg">Color:</span> {selectedColor}
+                            </div>
+                            <div className="mt-3 flex items-center gap-4">
+                                {product.colors[0].map(color => (
+                                    <div
+                                        key={color}
+                                        className="w-5 h-5 xl:h-6 xl:w-6 rounded-full cursor-pointer"
+                                        style={{
+                                            backgroundColor: color,
+                                            borderWidth: '2px',
+                                            boxShadow: selectedColor === color ? `0 0 0 1px ${color}` : '',
+                                        }}
+                                        onClick={() => setSelectedColor(color)}
+                                    ></div>
+                                ))}
                             </div>
                         </div>
-                    </div>
-                    <div className="flex-1">
-                        <div className="mt-8">
-                            {/* <div className="text-xs xl:text-base text-[#807D7E]">{product.category}</div> */}
-                            <div className="mt-4 md:mt-8 text-xl md:text-2xl xl:text-4xl text-[#3C4242] font-bold">{product.name}</div>
-                            <div className="mt-3 md:mt-6 text-[18px] md:text-[22px] text-3xl text-[#3C4242] font-medium">
-                                {product.oldPrice ? (
-                                    <div>
-                                        {`${formatCurrency(product.price)}`}
-                                        <span className="ml-3 text-[#848485] line-through" dangerouslySetInnerHTML={{ __html: `${formatCurrency(product.oldPrice)}` }}></span>
-                                    </div>
-                                ) : (
-                                    `${formatCurrency(product.price)}`
-                                )}
-                            </div>
-                            <div className="mt-4 flex items-center">
-                                <ProductStars
-                                    stars={product.stars}
-                                    ratings={product.ratings}
-                                />
-                                <div className="h-4 w-[1px] bg-black mx-3"></div>
-                                {product.inStock ? (
-                                    <div className="text-xs xl:text-green-500">In Stock</div>
-                                ) : (
-                                    <div className="text-xs xl:text-red-500">Out of Stock</div>
-                                )}
-                            </div>
-                            <div className="hidden lg:block mt-8 text-sm xl:text-base text-[#3C4242]">{product.description}</div>
-                            <div className="mt-8">
-                                <div><span className="font-bold text-[#3C4242] text-sm md:text-base xl:text-lg">Color:</span> {selectedColor}</div>
-                                <div className="mt-3 flex items-center gap-4">
-                                    {product.colors.map(color => (
-                                        <div
-                                            key={color}
-                                            className="w-5 h-5 xl:h-6 xl:w-6 rounded-full cursor-pointer"
-                                            style={{
-                                                backgroundColor: color,
-                                                borderWidth: '2px',
-                                                boxShadow: selectedColor === color ? `0 0 0 1px ${color}` : '',
-                                            }}
-                                            onClick={() => setSelectedColor(color)}
-                                        ></div>
-                                    ))}
-                                </div>
-                            </div>
-                            <div className="mt-8">
-                                {
-                                    product.sizes && (
-                                        <>
-                                            <div><span className="font-bold text-[#3C4242] text-sm md:text-base xl:text-lg">Size:</span> {selectedSize}</div>
-                                            <div className="mt-3 flex items-center gap-4">
-                                                {product.sizes.map(size => (
-                                                    <div
-                                                        key={size}
-                                                        className={cn(
-                                                            "px-3 py-2 flex items-center justify-center text-sm text-[#3C4242] border rounded-md cursor-pointer",
-                                                            selectedSize === size && 'bg-df-gray'
-                                                        )}
-                                                        onClick={() => setSelectedSize(size)}
-                                                    >{size}</div>
-                                                ))}
-                                            </div>
-                                        </>
-                                    )
-                                }
-                            </div>
-                            <Link href="#" className="block mt-6 text-[#3C4242] text-sm md:text-base xl:text-lg underline">View Size Guide</Link>
-                            <div className="mt-6 flex items-center gap-5">
-                                <div className="flex">
-                                    <div
-                                        className="w-10 h-10 xl:w-12 xl:h-12 flex items-center justify-center border cursor-pointer"
-                                        onClick={() => {
-                                            if (quantity > 1) {
-                                                setQuantity(current => current - 1)
-                                            }
-                                        }}
-                                    >
-                                        <FaMinus />
-                                    </div>
-                                    <div className="w-12 xl:w-14 h-10 xl:h-12 flex items-center justify-center border">
-                                        {quantity}
-                                    </div>
-                                    <div
-                                        className="w-10 h-10 xl:w-12 xl:h-12 flex items-center justify-center border cursor-pointer"
-                                        onClick={() => setQuantity(current => current + 1)}
-                                    >
-                                        <FaPlus />
-                                    </div>
-                                </div>
-                                <button
-                                    className="w-full md:w-[210px] h-10 xl:h-12 flex items-center justify-center gap-4 bg-black text-white text-xs md:text-sm font-light uppercase"
-                                    onClick={handleAddItem}
-                                    disabled={!quantity}
+                        <div className="mt-6">
+                            {
+                                product.sizes && (
+                                    <>
+                                        <div className="hidden lg:block"><span className="font-bold text-[#3C4242] text-sm md:text-base xl:text-lg">Size:</span> {selectedSize}</div>
+                                        <div className="mt-3 flex items-center gap-4">
+                                            {product.sizes[0].map(size => (
+                                                <div
+                                                    key={size}
+                                                    className={cn(
+                                                        "px-3 py-2 flex items-center justify-center text-sm text-[#3C4242] border rounded-md cursor-pointer",
+                                                        selectedSize === size && 'bg-df-gray'
+                                                    )}
+                                                    onClick={() => setSelectedSize(size)}
+                                                >{size}</div>
+                                            ))}
+                                        </div>
+                                    </>
+                                )
+                            }
+                        </div>
+                        <div className="mt-6 flex items-center gap-5">
+                            <div className="flex">
+                                <div
+                                    className="w-10 h-10 flex items-center justify-center border cursor-pointer"
+                                    onClick={() => {
+                                        if (quantity > 1) {
+                                            setQuantity(current => current - 1)
+                                        }
+                                    }}
                                 >
-                                    Add to cart <IoCartOutline size={18} />
-                                </button>
+                                    <FaMinus />
+                                </div>
+                                <div className="w-12 h-10 flex items-center justify-center border">
+                                    {quantity}
+                                </div>
+                                <div
+                                    className="w-10 h-10 flex items-center justify-center border cursor-pointer"
+                                    onClick={() => setQuantity(current => current + 1)}
+                                >
+                                    <FaPlus />
+                                </div>
                             </div>
+                            <button
+                                className="w-full md:w-[210px] h-10 flex items-center justify-center gap-4 bg-black text-white text-xs md:text-sm font-light uppercase"
+                                onClick={handleAddItem}
+                                disabled={!quantity}
+                            >
+                                Add to cart <IoCartOutline size={18} />
+                            </button>
                         </div>
                     </div>
                 </div>
