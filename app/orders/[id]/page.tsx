eebe2b7053c1ffb0a98dbc86b88370d791465bd1
Osememen Ogbe <osememen.ogbe@gmail.com>
2024-12-17 02:06:13 +0100

    added not found redirect

--- app/orders/[id]/page.tsx
+++ app/orders/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation"
+
 import Container from "@/components/Container"
 import Client from "./Client"
 
@@ -5,6 +7,10 @@ import { getOrder } from "@/lib/sanity"
 
 const OrderSummaryPage = async ({ params }: { params: { id: number } }) => {
     const order = await getOrder(String(params.id))
+
+    if (Object.keys(order).length === 0) {
+        notFound()
+    }
 
     return (
         <Container>
